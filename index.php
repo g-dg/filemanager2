d6b79ff5cd07c6f3d1bb3407c397c6dda0ff59fb
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2017-10-17 19:29:16 -0400

    More style fixes

--- index.php
+++ index.php
@@ -3,7 +3,11 @@ namespace GarnetDG\FileManager2;
 
 const VERSION = '2.0.0-dev';
 
-require_once( '_system/loader.php' );
+require_once('_system/config.md');
+Config::load();
+
+
+require_once('_system/loader.php');
 Loader::loadAll();
 
 Plugins::loadAll();
