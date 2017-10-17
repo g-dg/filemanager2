acbdcee5e92565f256b58e915f35af849331f0ad
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2017-10-17 19:37:32 -0400

    Fix to index.php and set up logging

--- index.php
+++ index.php
@@ -3,9 +3,11 @@ namespace GarnetDG\FileManager2;
 
 const VERSION = '2.0.0-dev';
 
-require_once('_system/config.md');
+require_once('_system/config.php');
 Config::load();
 
+require_once('_system/log.php');
+Log::setUp();
 
 require_once('_system/loader.php');
 Loader::loadAll();
