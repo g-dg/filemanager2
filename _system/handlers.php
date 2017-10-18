eacbc8bb2c76cac7656fad7f7844725790d55b5d
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2017-10-17 20:20:10 -0400

    Break handlers off into handlers.php

---
+++ _system/handlers.php
@@ -0,0 +1,33 @@
+<?php
+namespace GarnetDG\FileManager2;
+
+if (!defined('GARNETDG_FILEMANAGER2_VERSION')) {
+	http_response_code(403);
+	die();
+}
+
+class Handler
+{
+	protected static $registered_handlers = [];
+
+	public static function register($handler_name, $callback)
+	{
+		if (!isset(self::$registered_handlers[$handler_name])) {
+			self::$registered_handlers[$handler_name] = [$callback];
+		} else {
+			array_push(self::$registered_handlers[$handler_name], $callback);
+		}
+	}
+
+	public static function invoke($handler_name, $arguments, $last_only = false)
+	{
+		if ($last_only) {
+			call_user_func_array(self::$registered_handlers[$handler_name][count(self::$registered_handlers[$handler_name]) - 1], $arguments);
+		} else {
+			foreach($self::$registered_handlers[$handler_name] as $handler_function) {
+				call_user_func_array($handler_function, $arguments);
+			}
+		}
+	}
+
+}
--- _system/loader.php
+++ _system/loader.php
@@ -9,7 +9,6 @@ if (!defined('GARNETDG_FILEMANAGER2_VERSION')) {
 class Loader
 {
 	protected static $registered_inits = [];
-	protected static $registered_handlers = [];
 
 	public static function loadAll()
 	{
@@ -33,26 +32,6 @@ class Loader
 	public static function registerInit($init_function)
 	{
 		array_push(self::$registered_inits, $init_function);
-	}
-
-	public static function registerHandler($handler_name, $callback)
-	{
-		if (!isset(self::$registered_handlers[$handler_name])) {
-			self::$registered_handlers[$handler_name] = [$callback];
-		} else {
-			array_push(self::$registered_handlers[$handler_name], $callback);
-		}
-	}
-
-	public static function invokeHandlers($handler_name, $arguments, $last_only = false)
-	{
-		if ($last_only) {
-			call_user_func_array(self::$registered_handlers[$handler_name][count(self::$registered_handlers[$handler_name]) - 1], $arguments);
-		} else {
-			foreach($self::$registered_handlers[$handler_name] as $handler_function) {
-				call_user_func_array($handler_function, $arguments);
-			}
-		}
 	}
 
 }
