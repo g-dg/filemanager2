13a92c64bd4b42c07261a0bda886caa515f108cd
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2017-10-17 20:07:10 -0400

    Add code for handlers in loader.php

--- _system/loader.php
+++ _system/loader.php
@@ -9,6 +9,7 @@ if (!defined('GARNETDG_FILEMANAGER2_VERSION')) {
 class Loader
 {
 	protected static $registered_inits = [];
+	protected static $registered_handlers = [];
 
 	public static function loadAll()
 	{
@@ -36,12 +37,22 @@ class Loader
 
 	public static function registerHandler($handler_name, $callback)
 	{
-
+		if (!isset(self::$registered_handlers[$handler_name])) {
+			self::$registered_handlers[$handler_name] = [$callback];
+		} else {
+			array_push(self::$registered_handlers[$handler_name], $callback);
+		}
 	}
 
 	public static function invokeHandlers($handler_name, $arguments, $last_only = false)
 	{
-
+		if ($last_only) {
+			call_user_func_array(self::$registered_handlers[$handler_name][count(self::$registered_handlers[$handler_name]) - 1], $arguments);
+		} else {
+			foreach($self::$registered_handlers[$handler_name] as $handler_function) {
+				call_user_func_array($handler_function, $arguments);
+			}
+		}
 	}
 
 }
