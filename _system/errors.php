d967070e472c590e66781221234b770be94a06d0
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2017-10-18 08:28:33 -0400

    Add basic error pages

---
+++ _system/errors.php
@@ -0,0 +1,22 @@
+<?php
+namespace GarnetDG\FileManager2;
+
+if (!defined('GARNETDG_FILEMANAGER2_VERSION')) {
+	http_response_code(403);
+	die();
+}
+
+class ErrorPages
+{
+	public static function registerErrorPages()
+	{
+		Router::registerErrorPage(404,  __CLASS__ . '::errorPage');
+	}
+
+	public static function errorPage($http_error, $page)
+	{
+		echo 'Error: ' . $http_error . ' ' . $page;
+	}
+}
+
+Loader::registerInit(__NAMESPACE__ . '\ErrorPages::registerErrorPages');
