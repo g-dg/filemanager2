bdc8edfb421391f213a3bc8d3fedccd53fbe3364
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2017-10-18 07:55:09 -0400

    Add temporary error reporting to index.php

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 namespace GarnetDG\FileManager2;
 
+//TODO: remove error reporting for release
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+
 define('GARNETDG_FILEMANAGER2_VERSION', '2.0.0-dev');
 
 require_once('_system/config.php');
