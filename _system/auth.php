b69c1a4ad1211cebc5b4288543aa7b8a8b8ca21e
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2017-10-18 16:25:48 -0400

    Add basic stuff to auth.php

--- _system/auth.php
+++ _system/auth.php
@@ -8,14 +8,25 @@ if (!defined('GARNETDG_FILEMANAGER2_VERSION')) {
 
 class Authenticate
 {
+	protected static $authenticated = false;
+
 	// pass $username and $password to authenticate a new user
 	public static function authenticate($username = null, $password = null)
 	{
+		if (!self::$authenticated) {
+			Session::start();
 
+		}
+	}
+
+	public static function getCurrentUserId()
+	{
+		self::authenticate();
+		return Session::get('__user_id', null);
 	}
 
 	public static function logout($keep_session = true)
 	{
-
+		Session::unsetSession(false);
 	}
 }
