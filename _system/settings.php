99fd0db74337b943ef0095c21260841013b8070a
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2017-10-19 14:29:37 -0400

    Add $force parameter to settings functions

--- _system/settings.php
+++ _system/settings.php
@@ -8,7 +8,7 @@ if (!defined('GARNETDG_FILEMANAGER_VERSION')) {
 
 class GlobalSettings
 {
-	public static function get($key, $default = null)
+	public static function get($key, $default = null, $force = false)
 	{
 		// don't authenticate for this
 		Database::setLock(true);
@@ -21,21 +21,21 @@ class GlobalSettings
 		return $value;
 	}
 
-	public static function set($key, $value)
+	public static function set($key, $value, $force = false)
 	{
-		if (Auth::getCurrentUserType() >= Auth::USER_TYPE_ADMIN) {
+		if (Auth::getCurrentUserType() >= Auth::USER_TYPE_ADMIN || $force) {
 			Database::query('INSERT INTO "global_settings" ("key", "value") VALUES (?, ?);', [$key, $value]);
 		}
 	}
 
-	public static function isset($key)
+	public static function isset($key, $force = false)
 	{
 		return Database::query('SELECT COUNT() from "global_settings" WHERE "key" = ?;', [$key])[0][0] > 0;
 	}
 
-	public static function unset($key)
+	public static function unset($key, $force = false)
 	{
-		if (Auth::getCurrentUserType() >= Auth::USER_TYPE_ADMIN) {
+		if (Auth::getCurrentUserType() >= Auth::USER_TYPE_ADMIN || $force) {
 			Database::query('DELETE FROM "global_settings" WHERE "key" = ?;', [$key]);
 		}
 	}
@@ -43,12 +43,12 @@ class GlobalSettings
 
 class UserSettings
 {
-	public static function get($key, $default, $user = null)
+	public static function get($key, $default, $user = null, $force = false)
 	{
 		if (is_null($user)) {
 			$user = Auth::getCurrentUserId();
 		}
-		if ($user === Auth::getCurrentUserId() || Auth::getCurrentUserType >= Auth::USER_TYPE_ADMIN) {
+		if ($user === Auth::getCurrentUserId() || Auth::getCurrentUserType >= Auth::USER_TYPE_ADMIN || $force) {
 			Database::setLock(true);
 				if (self::isset($key, $user)) {
 					$value = Database::query('SELECT "value" FROM "user_settings" WHERE "key" = ? AND "user_id" = ?;', [$key, $user])[0][0];
@@ -60,32 +60,32 @@ class UserSettings
 		}
 	}
 
-	public static function set($key, $value, $user = null)
+	public static function set($key, $value, $user = null, $force = false)
 	{
 		if (is_null($user)) {
 			$user = Auth::getCurrentUserId();
 		}
-		if ($user === Auth::getCurrentUserId() || Auth::getCurrentUserType >= Auth::USER_TYPE_ADMIN) {
+		if ($user === Auth::getCurrentUserId() || Auth::getCurrentUserType >= Auth::USER_TYPE_ADMIN || $force) {
 			Database::query('INSERT INTO "user_settings" ("key", "user_id", "value") VALUES (?, ?, ?);', [$key, $user, $value]);
 		}
 	}
 
-	public static function isset($key, $user = null)
+	public static function isset($key, $user = null, $force = false)
 	{
 		if (is_null($user)) {
 			$user = Auth::getCurrentUserId();
 		}
-		if ($user === Auth::getCurrentUserId() || Auth::getCurrentUserType >= Auth::USER_TYPE_ADMIN) {
+		if ($user === Auth::getCurrentUserId() || Auth::getCurrentUserType >= Auth::USER_TYPE_ADMIN || $force) {
 			return Database::query('SELECT COUNT() from "user_settings" WHERE "key" = ? AND "user_id" = ?;', [$key, $user])[0][0] > 0;
 		}
 	}
 
-	public static function unset($key, $user = null)
+	public static function unset($key, $user = null, $force = false)
 	{
 		if (is_null($user)) {
 			$user = Auth::getCurrentUserId();
 		}
-		if ($user === Auth::getCurrentUserId() || Auth::getCurrentUserType >= Auth::USER_TYPE_ADMIN) {
+		if ($user === Auth::getCurrentUserId() || Auth::getCurrentUserType >= Auth::USER_TYPE_ADMIN || $force) {
 			Database::query('DELETE FROM "user_settings" WHERE "key" = ? AND "user_id" = ?;', [$key, $user]);
 		}
 	}
