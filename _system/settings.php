aee875ca5ea21be82b4636609fed549484ae27c8
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2017-10-19 12:18:36 -0400

    Add global settings code

--- _system/settings.php
+++ _system/settings.php
@@ -8,24 +8,36 @@ if (!defined('GARNETDG_FILEMANAGER2_VERSION')) {
 
 class GlobalSettings
 {
-	public static function get($key, $default)
+	public static function get($key, $default = null)
 	{
-
+		// don't authenticate for this
+		Database::setLock(true);
+		if (self::isset($key)) {
+			$value = Database::query('SELECT "value" FROM "global_settings" WHERE "key" = ?;', [$key])[0][0];
+		} else {
+			$value = $default;
+		}
+		Database::setLock(false);
+		return $value;
 	}
 
 	public static function set($key, $value)
 	{
-
+		if (Auth::getCurrentUserType() >= Auth::USER_TYPE_ADMIN) {
+			Database::query('INSERT INTO "global_settings" ("key", "value") VALUES (?, ?);', [$key, $value]);
+		}
 	}
 
 	public static function isset($key)
 	{
-
+		return Database::query('SELECT COUNT() from "global_settings" WHERE "key" = ?;', [$key])[0][0] > 0;
 	}
 
 	public static function unset($key)
 	{
-
+		if (Auth::getCurrentUserType() >= Auth::USER_TYPE_ADMIN) {
+			Database::query('DELETE FROM "global_settings" WHERE "key" = ?;', [$key]);
+		}
 	}
 }
 
