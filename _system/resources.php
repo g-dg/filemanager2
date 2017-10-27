4ff48008a7008174302327f40bf968627eb088cc
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2017-10-27 18:34:22 -0400

    Add Resources class

---
+++ _system/resources.php
@@ -0,0 +1,29 @@
+<?php
+namespace GarnetDG\FileManager;
+
+if (!defined('GARNETDG_FILEMANAGER_VERSION')) {
+	http_response_code(403);
+	die();
+}
+
+class Resources
+{
+	protected static $registered_resources = [];
+
+	public static function register($name, $callback)
+	{
+		if (!isset(self::$registered_resources[$name])) {
+			self::$registered_resources[$name] = $callback;
+		} else {
+			Log::error('The resource "' . $name . '" has already been registered');
+		}
+	}
+}
+
+Router::registerPage('res', function($resource) {
+	if (isset(self::$registered_resources[$resource])) {
+		call_user_func($registered_resources[$resource]);
+	} else {
+		Router::execErrorPage(404);
+	}
+});
