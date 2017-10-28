18736ef91974435f67ef3cc1ba986ffe89d82c78
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2017-10-28 12:05:11 -0400

    Add basic upload page

---
+++ _plugins/main/upload.php
@@ -0,0 +1,12 @@
+<?php
+namespace GarnetDG\FileManager;
+
+if (!defined('GARNETDG_FILEMANAGER_VERSION')) {
+	http_response_code(403);
+	die();
+}
+
+Router::registerPage('upload', function($path) {
+	Auth::authenticate();
+	
+});
