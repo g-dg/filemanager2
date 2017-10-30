6c0e5cf95d86c769488a805655d5517fcd7132dc
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2017-10-30 15:23:55 -0400

    Still more work on the main UI (I can see a pattern forming...)

--- _plugins/main/admin.php
+++ _plugins/main/admin.php
@@ -35,7 +35,7 @@ Router::registerPage('admin', function($subpage) {
 				MainUiTemplate::head('Users - Administration', '<link rel="stylesheet" href="' . Router::getHttpReadyUri('/resource/main/admin.css') . '" type="text/css" />');
 				echo '
 	<fieldset><legend>Create User</legend>
-		<form action="'.Router::getHttpReadyUri('/admin/action/users').'">
+		<form action="'.Router::getHttpReadyUri('/admin/action/users').'" method="post">
 			<div class="row">
 				<div class="six columns">
 					<label for="create_username">Username:</label>
@@ -69,7 +69,7 @@ Router::registerPage('admin', function($subpage) {
 			<div class="row">
 				<div class="twelve columns">
 					<label for="create">Create User:</label>
-					<input id="create" name="create" type="submit" value="Create User" class="u-full-width" onclick="return confirm(\'Create user &quot;\'+document.getElementById(\'create_username\').value+\'&quot;?\');" />
+					<input id="create" name="create" type="submit" value="Create User" class="u-full-width" />
 					<input id="create_cancel" name="reset" type="reset" value="Cancel" class="u-full-width" />
 				</div>
 			</div>
@@ -92,9 +92,10 @@ Router::registerPage('admin', function($subpage) {
 					echo '					';
 
 					echo '<form action="'.Router::getHttpReadyUri('/admin/action/users/'.(int)$user_id).'" method="post">';
+
 					echo '<div>';
-					echo '<input id="username_'.htmlspecialchars($user_id).'" name="username" type="text" value="'.htmlspecialchars(Users::getName($user_id)).'" placeholder="Username" />';
-					echo '<input id="update_username_'.htmlspecialchars($user_id).'" name="update_name" type="submit" value="Change Username" />';
+					echo '<input id="name_'.htmlspecialchars($user_id).'" name="name" type="text" value="'.htmlspecialchars(Users::getName($user_id)).'" placeholder="Name" />';
+					echo '<input id="update_name_'.htmlspecialchars($user_id).'" name="update_name" type="submit" value="Change Name" />';
 					echo '</div>';
 
 					echo '<div>';
@@ -131,12 +132,12 @@ Router::registerPage('admin', function($subpage) {
 					echo '</div>';
 
 					echo '<div>';
-					echo '<textarea id="comment_'.htmlspecialchars($user_id).'" name="comment['.htmlspecialchars($user_id).']" placeholder="Comment">'.htmlspecialchars(Users::getComment($user_id)).'</textarea>';
+					echo '<textarea id="comment_'.htmlspecialchars($user_id).'" name="comment" placeholder="Comment">'.htmlspecialchars(Users::getComment($user_id)).'</textarea>';
 					echo '<input id="update_comment_'.htmlspecialchars($user_id).'" name="update_comment" type="submit" value="Update Comment" />';
 					echo '</div>';
 
 					echo '<div>';
-					echo '<input id="delete_'.htmlspecialchars($user_id).'" name="delete" type="submit" value="Delete" onclick="return confirm(\'Delete user &quot;\'+document.getElementById(\'username_'.htmlspecialchars($user_id).'\').getAttribute(\'value\')+\'&quot;?\');" />';
+					echo '<input id="delete_'.htmlspecialchars($user_id).'" name="delete" type="submit" value="Delete" onclick="return confirm(\'Delete user &quot;\'+document.getElementById(\'name_'.htmlspecialchars($user_id).'\').getAttribute(\'value\')+\'&quot;?\');" />';
 					echo '</div>';
 
 					echo '<div>';
@@ -165,8 +166,84 @@ Router::registerPage('admin', function($subpage) {
 
 
 			case 'groups':
-				MainUiTemplate::head('Groups - Administration');
+				MainUiTemplate::head('Groups - Administration', '<link rel="stylesheet" href="' . Router::getHttpReadyUri('/resource/main/admin.css') . '" type="text/css" />');
+				echo '
+	<fieldset><legend>Create Group</legend>
+		<form action="'.Router::getHttpReadyUri('/admin/action/groups').'" method="post">
+			<div class="row">
+				<div class="four columns">
+					<label for="create_name">Name:</label>
+					<input id="create_name" name="name" type="text" value="" placeholder="Name" class="u-full-width" />
+				</div>
+				<div class="four columns">
+					<label for="create_enabled">Enabled:</label>
+					<select id="create_enabled" name="enabled" class="u-full-width"><option value="enabled" selected="selected">Enabled</option><option value="disabled">Disabled</option></select>
+				</div>
+				<div class="four columns">
+					<label for="create_comment">Comment:</label>
+					<textarea id="create_comment" name="comment" placeholder="Comment" class="u-full-width"></textarea>
+				</div>
+			</div>
+			<div class="row">
+				<div class="twelve columns">
+					<label for="create">Create Group:</label>
+					<input id="create" name="create" type="submit" value="Create Group" class="u-full-width" />
+					<input id="create_cancel" name="reset" type="reset" value="Cancel" class="u-full-width" />
+				</div>
+			</div>
+		</form>
+	</fieldset>
+	<div class="overflow">
+		<fieldset><legend>Groups</legend>
+			<div class="table">
+				<div class="thead">
+					<div>Name</div>
+					<div>Enabled</div>
+					<div>Comment</div>
+					<div>Delete</div>
+					<div>Cancel</div>
+				</div>
+';
+				foreach (Groups::getAll() as $group_id) {
+					echo '					';
 
+					echo '<form action="'.Router::getHttpReadyUri('/admin/action/groups/'.(int)$group_id).'" method="post">';
+
+					echo '<div>';
+					echo '<input id="name_'.htmlspecialchars($group_id).'" name="name" type="text" value="'.htmlspecialchars(Groups::getName($group_id)).'" placeholder="Name" />';
+					echo '<input id="update_name_'.htmlspecialchars($group_id).'" name="update_name" type="submit" value="Change Name" />';
+					echo '</div>';
+
+					echo '<div>';
+					if (Groups::getEnabled($group_id)) {
+						echo '<select id="enabled_'.htmlspecialchars($group_id).'" name="enabled"><option value="enabled" selected="selected">Enabled</option><option value="disabled">Disabled</option></select>';
+					} else {
+						echo '<select id="enabled_'.htmlspecialchars($group_id).'" name="enabled"><option value="enabled">Enabled</option><option value="disabled" selected="selected">Disabled</option></select>';
+					}
+					echo '<input id="update_enabled_'.htmlspecialchars($group_id).'" name="update_enabled" type="submit" value="Set Enabled" />';
+					echo '</div>';
+
+					echo '<div>';
+					echo '<textarea id="comment_'.htmlspecialchars($group_id).'" name="comment" placeholder="Comment">'.htmlspecialchars(Groups::getComment($group_id)).'</textarea>';
+					echo '<input id="update_comment_'.htmlspecialchars($group_id).'" name="update_comment" type="submit" value="Update Comment" />';
+					echo '</div>';
+
+					echo '<div>';
+					echo '<input id="delete_'.htmlspecialchars($group_id).'" name="delete" type="submit" value="Delete" onclick="return confirm(\'Delete group &quot;\'+document.getElementById(\'name_'.htmlspecialchars($group_id).'\').getAttribute(\'value\')+\'&quot;?\');" />';
+					echo '</div>';
+
+					echo '<div>';
+					echo '<input id="cancel_'.htmlspecialchars($group_id).'" name="reset" type="reset" value="Cancel" />';
+					echo '</div>';
+
+					echo '</form>';
+
+					echo PHP_EOL;
+				}
+				echo '				</div>
+		</fieldset>
+	</div>
+';
 				MainUiTemplate::foot();
 				break;
 
@@ -181,8 +258,94 @@ Router::registerPage('admin', function($subpage) {
 
 
 			case 'shares':
-				MainUiTemplate::head('Shares - Administration');
+				MainUiTemplate::head('Shares - Administration', '<link rel="stylesheet" href="' . Router::getHttpReadyUri('/resource/main/admin.css') . '" type="text/css" />');
+				echo '
+	<fieldset><legend>Create Share</legend>
+		<form action="'.Router::getHttpReadyUri('/admin/action/shares').'" method="post">
+			<div class="row">
+				<div class="three columns">
+					<label for="create_name">Name:</label>
+					<input id="create_name" name="name" type="text" value="" placeholder="Name" class="u-full-width" />
+				</div>
+				<div class="three columns">
+					<label for="create_path">Path:</label>
+					<input id="create_path" name="path" type="text" value="" placeholder="Path" class="u-full-width" />
+				</div>
+				<div class="three columns">
+					<label for="create_enabled">Enabled:</label>
+					<select id="create_enabled" name="enabled" class="u-full-width"><option value="enabled" selected="selected">Enabled</option><option value="disabled">Disabled</option></select>
+				</div>
+				<div class="three columns">
+					<label for="create_comment">Comment:</label>
+					<textarea id="create_comment" name="comment" placeholder="Comment" class="u-full-width"></textarea>
+				</div>
+			</div>
+			<div class="row">
+				<div class="twelve columns">
+					<label for="create">Create Share:</label>
+					<input id="create" name="create" type="submit" value="Create Share" class="u-full-width" />
+					<input id="create_cancel" name="reset" type="reset" value="Cancel" class="u-full-width" />
+				</div>
+			</div>
+		</form>
+	</fieldset>
+	<div class="overflow">
+		<fieldset><legend>Shares</legend>
+			<div class="table">
+				<div class="thead">
+					<div>Name</div>
+					<div>Path</div>
+					<div>Enabled</div>
+					<div>Comment</div>
+					<div>Delete</div>
+					<div>Cancel</div>
+				</div>
+';
+				foreach (Shares::getAll() as $share_id) {
+					echo '					';
 
+					echo '<form action="'.Router::getHttpReadyUri('/admin/action/groups/'.(int)$share_id).'" method="post">';
+
+					echo '<div>';
+					echo '<input id="name_'.htmlspecialchars($share_id).'" name="name" type="text" value="'.htmlspecialchars(Shares::getName($share_id)).'" placeholder="Name" />';
+					echo '<input id="update_name_'.htmlspecialchars($share_id).'" name="update_name" type="submit" value="Change Name" />';
+					echo '</div>';
+
+					echo '<div>';
+					echo '<input id="path_'.htmlspecialchars($share_id).'" name="path" type="text" value="'.htmlspecialchars(Shares::getPath($share_id)).'" placeholder="Name" />';
+					echo '<input id="update_path_'.htmlspecialchars($share_id).'" name="update_path" type="submit" value="Change Path" />';
+					echo '</div>';
+
+					echo '<div>';
+					if (Shares::getEnabled($share_id)) {
+						echo '<select id="enabled_'.htmlspecialchars($share_id).'" name="enabled"><option value="enabled" selected="selected">Enabled</option><option value="disabled">Disabled</option></select>';
+					} else {
+						echo '<select id="enabled_'.htmlspecialchars($share_id).'" name="enabled"><option value="enabled">Enabled</option><option value="disabled" selected="selected">Disabled</option></select>';
+					}
+					echo '<input id="update_enabled_'.htmlspecialchars($share_id).'" name="update_enabled" type="submit" value="Set Enabled" />';
+					echo '</div>';
+
+					echo '<div>';
+					echo '<textarea id="comment_'.htmlspecialchars($share_id).'" name="comment" placeholder="Comment">'.htmlspecialchars(Shares::getComment($share_id)).'</textarea>';
+					echo '<input id="update_comment_'.htmlspecialchars($share_id).'" name="update_comment" type="submit" value="Update Comment" />';
+					echo '</div>';
+
+					echo '<div>';
+					echo '<input id="delete_'.htmlspecialchars($share_id).'" name="delete" type="submit" value="Delete" onclick="return confirm(\'Delete share &quot;\'+document.getElementById(\'name_'.htmlspecialchars($share_id).'\').getAttribute(\'value\')+\'&quot;?\');" />';
+					echo '</div>';
+
+					echo '<div>';
+					echo '<input id="cancel_'.htmlspecialchars($share_id).'" name="reset" type="reset" value="Cancel" />';
+					echo '</div>';
+
+					echo '</form>';
+
+					echo PHP_EOL;
+				}
+				echo '				</div>
+		</fieldset>
+	</div>
+';
 				MainUiTemplate::foot();
 				break;
 
