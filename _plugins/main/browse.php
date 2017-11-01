98f293d7f392b3d92b737fa5e83b2bb39e323bee
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2017-11-01 18:17:06 -0400

    Improve directory listing

--- _plugins/main/browse.php
+++ _plugins/main/browse.php
@@ -9,33 +9,45 @@ if (!defined('GARNETDG_FILEMANAGER_VERSION')) {
 Router::registerPage('browse', function($path) {
 	Auth::authenticate();
 	MainUiTemplate::header('/' . $path);
-	echo '		<div class="overflow"><table class="u-full-width"><thead><tr><th>Name</th><th>Last Modified</th><th>Size</th></tr></thead><tbody>';
+	echo '		<div class="overflow"><table class="u-full-width"><thead><tr><th>Name</th><th>Last Modified</th><th>Size</th><th>Download</th></tr></thead><tbody>';
 	$dirlist = Filesystem::scandir($path);
 	if ($dirlist) {
+		natcasesort($dirlist);
+		if ($path !== '') {
+			echo '<tr><td><a href="..">[Parent Directory]</a></td><td></td><td></td><td></td></tr>';
+		}
 		foreach ($dirlist as $file) {
-			echo '<tr>';
+			if (substr($file, 0, 1) !== '.') {
+				echo '<tr>';
 
-			echo '<td>';
-			if (Filesystem::is_dir($path.'/'.$file)) {
-				echo '<a href="'.Router::getHtmlReadyUri('/browse/'.$path.'/'.$file).'">'.htmlspecialchars($file).'</a><br/>';
-			} else {
-				echo '<a href="'.Router::getHtmlReadyUri('/file/'.Session::getSessionId().'/'.$path.'/'.$file).'" target="_blank">'.htmlspecialchars($file).'</a><br/>';
+				echo '<td>';
+				if (Filesystem::is_dir($path.'/'.$file)) {
+					echo '<a href="'.Router::getHtmlReadyUri('/browse/'.$path.'/'.$file).'/">'.htmlspecialchars($file).'</a><br/>';
+				} else {
+					echo '<a href="'.Router::getHtmlReadyUri('/file/'.Session::getSessionId().'/'.$path.'/'.$file).'" target="_blank">'.htmlspecialchars($file).'</a><br/>';
+				}
+				echo '</td>';
+
+				echo '<td>';
+				echo date('Y-m-d H:i', Filesystem::filemtime($path.'/'.$file));
+				echo '</td>';
+
+				echo '<td>';
+				if (Filesystem::is_dir($path.'/'.$file)) {
+					echo Filesystem::fileCount($path.'/'.$file);
+				} else {
+					echo Filesystem::filesize($path.'/'.$file);
+				}
+				echo '</td>';
+
+				echo '<td>';
+				if (Filesystem::is_file($path.'/'.$file)) {
+					echo '<a href="'.Router::getHtmlReadyUri('/download/'.$path.'/'.$file).'">Download</a>';
+				}
+				echo '</td>';
+
+				echo '</tr>';
 			}
-			echo '</td>';
-
-			echo '<td>';
-			echo date('Y-m-d H:i', Filesystem::filemtime($path.'/'.$file));
-			echo '</td>';
-
-			echo '<td>';
-			if (Filesystem::is_dir($path.'/'.$file)) {
-				echo Filesystem::fileCount($path.'/'.$file);
-			} else {
-				echo Filesystem::filesize($path.'/'.$file);
-			}
-			echo '</td>';
-
-			echo '</tr>';
 		}
 	}
 	echo '</tbody></table></div>';
