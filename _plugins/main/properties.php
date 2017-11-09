ebed1ec92060a8cbe9e66f6d7727f5c3a53a1a2b
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2017-11-09 16:25:24 -0500

    Complete the properties page

--- _plugins/main/properties.php
+++ _plugins/main/properties.php
@@ -8,7 +8,124 @@ if (!defined('GARNETDG_FILEMANAGER_VERSION')) {
 
 Router::registerPage('properties', function($path) {
 	Auth::authenticate();
-	MainUiTemplate::header('Properties of /'.$path);
+	MainUiTemplate::header('Properties of /'.$path, '<link rel="stylesheet" href="' . Router::getHtmlReadyUri('/resource/main/properties.css') . '" type="text/css" />');
+
+	echo '<a href="'.Router::getHtmlReadyUri(pathinfo('/browse/'.$path)['dirname']).'">&lt; Back</a>';
+
+	echo '<fieldset><legend>Properties</legend>';
+	echo '<h3 style="margin-top: .5em;">'.htmlspecialchars('/'.$path).'</h3>';
+
+	echo '<table><thead><tr><th>Property</th><th>Value</th></tr></thead><tbody>';
+
+
+	echo '<tr><td>Type:</td><td>';
+	$filetype = Filesystem::filetype($path);
+	switch ($filetype) {
+		case 'file':
+			echo 'File';
+			break;
+		case 'dir':
+			echo 'Folder';
+			break;
+		default:
+			echo 'Other/Unknown ('.htmlspecialchars($filetype).')';
+			break;
+	}
+	echo '</td></tr>';
+
+
+	echo '<tr><td>Size:</td><td>';
+	if (Filesystem::is_dir($path)) {
+		$filecount = Filesystem::fileCount($path);
+		if ($filecount < 1000) {
+			echo sprintf("%d file(s)", $filecount);
+		} else if ($filecount < 1000000) {
+			echo sprintf("%01.1f K files (%s files)", ($filecount / 1000), number_format($filecount, 0, '.', ','));
+		} else if ($filecount < 1000000000) {
+			echo sprintf("%01.1f M files (%s files)", ($filecount / 1000000), number_format($filecount, 0, '.', ','));
+		} else {
+			echo sprintf("%01.1f G files (%s files)", ($filecount / 1000000000), number_format($filecount, 0, '.', ','));
+		}
+	} else {
+		$filesize = Filesystem::filesize($path);
+		if ($filesize != 1) {
+			echo '<span id="file_size" title="'.htmlspecialchars(number_format($filesize, 0, '.', ',')).' bytes" onclick="alert(document.getElementById(\'file_size\').getAttribute(\'title\'));">';
+		} else {
+			echo '<span id="file_size" title="'.htmlspecialchars($filesize).' byte" onclick="alert(document.getElementById(\'file_size\').getAttribute(\'title\'));">';
+		}
+		if ($filesize < 1024) {
+			echo sprintf("%d byte(s)", $filesize);
+		} else if ($filesize < 1048576) {
+			echo sprintf("%01.1f KiB (%s bytes)", ($filesize / 1024), number_format($filesize, 0, '.', ','));
+		} else if ($filesize < 1073741824) {
+			echo sprintf("%01.1f MiB (%s bytes)", ($filesize / 1048576), number_format($filesize, 0, '.', ','));
+		} else if ($filesize < 1099511627776) {
+			echo sprintf("%01.1f GiB (%s bytes)", ($filesize / 1073741824), number_format($filesize, 0, '.', ','));
+		} else {
+			echo sprintf("%01.1f TiB (%s bytes", ($filesize / 1099511627776), number_format($filesize, 0, '.', ','));
+		}
+	}
+	echo '</td></tr>';
+
+
+	echo '<tr><td>Last Modified:</td><td>';
+	$mtime = Filesystem::filemtime($path);
+	$mtimediff = time() - $mtime;
+	if ($mtimediff < 0) {
+		echo 'In the future';
+	} else if ($mtimediff < 60) {
+		$seconds = $mtimediff;
+		if ($seconds != 1) {
+			echo $seconds . ' seconds ago';
+		} else {
+			echo '1 second ago';
+		}
+	} else if ($mtimediff < 3600) {
+		$minutes = floor($mtimediff / 60);
+		if ($minutes != 1) {
+			echo $minutes . ' minutes ago';
+		} else {
+			echo '1 minute ago';
+		}
+	} else if ($mtimediff < 86400) {
+		$hours = floor($mtimediff / 3600);
+		if ($hours != 1) {
+			echo $hours . ' hours ago';
+		} else {
+			echo '1 hour ago';
+		}
+	} else if ($mtimediff < 2592000) {
+		$days = floor($mtimediff / 86400);
+		if ($days != 1) {
+			echo $days . ' days ago';
+		} else {
+			echo '1 day ago';
+		}
+	} else if ($mtimediff < 31536000) {
+		$months = floor($mtimediff / 2592000);
+		if ($months != 1) {
+			echo $months . ' months ago';
+		} else {
+			echo '1 month ago';
+		}
+	} else {
+		$years = floor($mtimediff / 31536000);
+		if ($years != 1) {
+			echo $years . ' years ago';
+		} else {
+			echo '1 year ago';
+		}
+	}
+	echo ' ('.htmlspecialchars(date('l, F j, Y - g:i:s A', $mtime)).')';
+	echo '</td></tr>';
+
+
+	echo '</tbody></table></fieldset>';
+
 
 	MainUiTemplate::footer();
+});
+
+Resources::register('main/properties.css', function(){
+	Resources::serveFile('_plugins/main/resources/properties.css');
 });
