71be038c3a3bf9dd7fb41377027151b8f849363c
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2017-11-14 21:33:41 -0500

    Bump version to 2.0.0-alpha.8

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ namespace GarnetDG\FileManager;
 //error_reporting(E_ALL);
 //ini_set('display_errors', 'On');
 
-define('GARNETDG_FILEMANAGER_VERSION', '2.0.0-alpha.7');
+define('GARNETDG_FILEMANAGER_VERSION', '2.0.0-alpha.8');
 define('GARNETDG_FILEMANAGER_COPYRIGHT', 'Copyright &copy; 2017 Garnet DeGelder');
 
 set_time_limit(3600);
