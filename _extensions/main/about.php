a689c289c508c344ff410d9052c705556183c31d
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2017-12-01 10:19:35 -0500

    Relicense to LGPL-3

--- _extensions/main/about.php
+++ _extensions/main/about.php
@@ -18,16 +18,16 @@ Router::registerPage('about', function($subpage) {
 <p>'.GARNETDG_FILEMANAGER_COPYRIGHT.'</p>
 
 <p>This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
+it under the terms of the GNU Lesser General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.</p>
 
 <p>This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.</p>
+GNU Lesser General Public License for more details.</p>
 
-<p>You should have received a copy of the GNU General Public License
+<p>You should have received a copy of the GNU Lesser General Public License
 along with this program.  If not, see &lt;<a href="http://www.gnu.org/licenses/" target="_blank">http://www.gnu.org/licenses/</a>&gt;.</p>
 
 <hr />
