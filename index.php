29335824bec7aa58983e7780efa29cd5081b2fef
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2018-01-01 08:21:06 -0500

    Update copyright for 2018

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 namespace GarnetDG\FileManager;
 
 define('GARNETDG_FILEMANAGER_VERSION', '2.3.0-dev');
-define('GARNETDG_FILEMANAGER_COPYRIGHT', 'Copyright &copy; 2017 Garnet DeGelder');
+define('GARNETDG_FILEMANAGER_COPYRIGHT', 'Copyright &copy; 2017-2018 Garnet DeGelder');
 
 set_time_limit(3600);
 ignore_user_abort(true);
