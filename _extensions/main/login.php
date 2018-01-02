81842927df1f646d1ede9c773a7e2383c7194810
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2018-01-02 14:38:48 -0500

    Remove X-UA-Compatible

--- _extensions/main/login.php
+++ _extensions/main/login.php
@@ -14,7 +14,6 @@ Router::registerPage('login', function($subpage) {
 <head>
 	<meta charset="UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<meta http-equiv="X-UA-Compatible" content="ie=edge" />
 	<link rel="stylesheet" href="' . Router::getHtmlReadyUri('/resource/main/css/normalize.css') . '" type="text/css" />
 	<link rel="stylesheet" href="' . Router::getHtmlReadyUri('/resource/main/css/main.css') . '" type="text/css" />
 	<link rel="stylesheet" href="' . Router::getHtmlReadyUri('/resource/main/css/forms.css') . '" type="text/css" />
--- _extensions/main/template.php
+++ _extensions/main/template.php
@@ -21,7 +21,6 @@ class MainUiTemplate
 <head>
 	<meta charset="UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<meta http-equiv="X-UA-Compatible" content="ie=edge" />
 	<link rel="stylesheet" href="' . Router::getHtmlReadyUri('/resource/main/css/normalize.css') . '" type="text/css" />
 	<link rel="stylesheet" href="' . Router::getHtmlReadyUri('/resource/main/css/main.css') . '" type="text/css" />
 	<link rel="stylesheet" href="' . Router::getHtmlReadyUri('/resource/main/css/layout.css') . '" type="text/css" />
