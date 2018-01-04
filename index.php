93d8f889c7f0b30f1e06e257562560429e79bb9c
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2018-01-04 14:08:04 -0500

    Release 2.2.3

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 namespace GarnetDG\FileManager;
 
-define('GARNETDG_FILEMANAGER_VERSION', '2.2.2');
+define('GARNETDG_FILEMANAGER_VERSION', '2.2.3');
 define('GARNETDG_FILEMANAGER_COPYRIGHT', 'Copyright &copy; 2017 Garnet DeGelder');
 
 set_time_limit(3600);
